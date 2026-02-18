76205f4925ea375192394eee42bae6f42351da23
stefanappdev <stevendking@protonmail.com>
2026-02-18 00:29:13 -0800

    changed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ function Navbar() {
 
   const regmobileNavStyle={
        margin:'1rem auto',
-       color:'rgb(131, 129, 126)',
-      
+       color:'rgba(236, 233, 228, 1)',
+      textAlign:'center',
   }
 
   const activemobileNavStyle={
@@ -16,7 +16,8 @@ function Navbar() {
     backgroundColor:'white',
     margin:'1rem auto',
     fontWeight:'bold',
-    
+    color:'black',
+    textAlign:'center',
   }
 
   return (
@@ -27,19 +28,21 @@ function Navbar() {
 
         <NavLink style={({isActive})=>isActive?activemobileNavStyle:regmobileNavStyle}  to='/'>
             <img className={`  ${NavbarStyles['mobile-navImage']}`} src="../../images/icon-home.png"/>
-          
+            <span>Home</span>
         </NavLink> 
           
             
             <NavLink style={({isActive})=>isActive?activemobileNavStyle:regmobileNavStyle} to='/create'>
               <img className={`  ${NavbarStyles['mobile-navImage']}`} src="../../images/icon-plus.png"/>
-              
+              <span> create a note</span>
             </NavLink>
            
           
           
           <NavLink style={({isActive})=>isActive?activemobileNavStyle:regmobileNavStyle} to='/settings'>
              <img className={`   ${NavbarStyles['mobile-navImage']}`} src="../../images/icon-settings.png"/>
+              
+            <span> settings</span>
           </NavLink>
           
       </nav>
